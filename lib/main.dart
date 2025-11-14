8c8b869e845dba99b02ea3f9fbe9d1ad831b4f6c
massinissa mameri <massilockwood93@gmail.com>
2025-11-14 01:18:17 -0500

    ajout d'un container qui contient une column

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,39 @@ class MainApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.blueAccent, 
           ),
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              
+              Text(
+                'Bienvenue dans cet atelier !',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Image.network('https://picsum.photos/200',
+              width: 200,
+              ),
+              
+              SizedBox(height: 20),
+
+
+              Text(
+                'Decouvrez comment construire une interface simple et elegante',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              )
+            ],
+          ),
         ),
-      ),
-    );
+        ),
+      );
   }
 }
